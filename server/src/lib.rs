f8e25c092b2d58be240417a768625cea5709f156
cc0 <101407887+hslv@users.noreply.github.com>
2024-01-14 15:49:20 +0200

    backend tls

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,8 +1,11 @@
 mod routes;
 
+use axum_server::tls_rustls::RustlsConfig;
 use routes::routes;
 
 use std::net::SocketAddr;
+
+use clap::Parser;
 
 use axum::{ServiceExt, extract::FromRef};
 use dotenvy_macro::dotenv;
@@ -15,9 +18,39 @@ pub struct State {
     pub main: DatabaseConnection,
 }
 
+// Setup the cli with clap
+
+#[derive(Parser, Debug, Clone)]
+pub struct Opt {
+    // LOG LEVEL
+    #[clap(short = 'l', long = "log", default_value = "debug")]
+    log_level: String,
+    
+    // ADDR
+    #[clap(short = 'a', long = "addr", default_value = "0.0.0.0")]
+    addr: String,
+    
+    // PORT
+    #[clap(short = 'p', long = "port", default_value = "5000")]
+    port: String,
+
+    // Where frontend files are located (./dist)
+    #[clap(long = "static-dir", default_value = "./dist")]
+    static_dir: String,
+
+    // CERT.PEM
+    #[clap(long = "cert-path", default_value = "../cert.pem")]
+    cert_path: String,
+
+    // KEY.PEM
+    #[clap(long = "key-path", default_value = "../key.pem")]
+    key_path: String,
+}
 
 pub async fn run(database_url: &str) {
     
+    let opt = Opt::parse();
+
     // enable console logging
     tracing_subscriber::fmt::init();
 
@@ -25,7 +58,13 @@ pub async fn run(database_url: &str) {
     let state = State { main: main_database };
 
     let sock_addr: SocketAddr = format!("{}:{}",
-        dotenv!("SERVER_ADDR"), dotenv!("SERVER_PORT")).parse().unwrap();
+        opt.addr, opt.port).parse().unwrap();
+
+    // TLS
+    let config = RustlsConfig::from_pem_file(
+        opt.cert_path,
+        opt.key_path,
+    ).await.unwrap();
 
     // NormalizePathLayer allowes `...\user` and `...\user\` be the same path
     let app = NormalizePathLayer::trim_trailing_slash()
@@ -33,8 +72,14 @@ pub async fn run(database_url: &str) {
 
     log::info!("LISTENING on http://{}", &sock_addr);
 
-    axum::Server::bind(&sock_addr)
+    // axum::Server::bind(&sock_addr)
+    //     .serve(app.into_make_service())
+    //     .await
+    //     .expect("Unable to start the server, please check the config");
+
+    // // TLS support
+    axum_server::bind_rustls(sock_addr, config)
         .serve(app.into_make_service())
         .await
-        .expect("Unable to start the server, please check the config");
+        .expect("Unable to start the server");
 }
