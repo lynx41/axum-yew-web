703038eceaed365da0d2a360a4a219ea9655cece
cc0 <101407887+hslv@users.noreply.github.com>
2024-02-07 13:34:58 +0200

    header visual finished

--- frontend/src/components/parts/header/actions/mod.rs
+++ frontend/src/components/parts/header/actions/mod.rs
@@ -1,10 +1,15 @@
 mod language;
 mod orders;
 mod notifications;
+mod basket;
+mod user;
+
 
 use language::Languages;
 use orders::Orders;
 use notifications::Notifications;
+use basket::Basket;
+use user::User;
 
 use yew::{function_component, html, Html};
 
@@ -15,17 +20,44 @@ pub fn actions() -> Html {
         <ul class="header-actions">
         
         // default language can be changed in /src/stores/language.rs
+        // visible for everyone
         <Languages />
 
+
+        // development in plans, but for now 
+        // visible only for logged
         // <Help /> Only for non-auth users
 
-        // !!! Next components visible only for auth users
-        
-        <Orders />
 
+        // visible only for non-logged
+        <User />
+
+
+        // development in plans, but for now 
+        // visible only for logged
         // <Bonuses />
 
+
+        // redirects to special page of all user's orders
+        // visible only for logged
+        <Orders />
+
+
+        // visible only for logged
         <Notifications />
+
+
+        // development in plans, but for now 
+        // visible for everyone
+        // <Comparison />
+
+        // development in plans, but for now 
+        // visible only for logged
+        // <WishList />
+
+
+        // visible for everyone
+        <Basket />
 
         </ul>
     }
