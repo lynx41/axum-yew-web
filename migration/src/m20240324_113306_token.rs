be76457d636ca3b55b5dd14983634b12c4e80ae8
cc0 <101407887+hslv@users.noreply.github.com>
2024-03-24 13:52:29 +0200

    token migration

--- migration/src/m20240324_113306_token.rs
+++ migration/src/m20240324_113306_token.rs
@@ -54,7 +54,7 @@ impl MigrationTrait for Migration {
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         // Replace the sample below with your own migration scripts
-        
+
         manager
             .drop_foreign_key(
                 ForeignKey::drop()
